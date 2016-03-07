9c5f5782dd47272d145aba032edaa039de5b3757
Anton Ostapenko <olsnod@gmail.com>
2016-03-07 13:32:52 +0500

    Change type of url field to URLField =)

--- db/models.py
+++ db/models.py
@@ -8,11 +8,11 @@ class Site(models.Model):
 class News(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
     title = models.CharField(max_length=300)
-    url = models.CharField(max_length=300)
+    url = models.URLField(max_length=300)
     pub_date = models.DateTimeField('date published')
     summary = models.TextField(blank=True, null=True)
 
 
 class RssChannels(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
-    url = models.CharField(max_length=300)
+    url = models.URLField(max_length=300)
