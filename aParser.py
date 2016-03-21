02d9e9aa4dbb5231e66e7b1435de97b89cf883b4
Anton Ostapenko <olsnod@gmail.com>
2016-03-21 23:25:14 +0500

    add lazy method

--- aParser.py
+++ aParser.py
@@ -6,40 +6,44 @@ django.setup()
 
 from prjparser import urlOpen, textParser
 import html
+import re
 
-test_url = "http://news.rambler.ru/"
+
+test_url = "https://meduza.io/"
 
 txt = urlOpen.read(test_url)
 txt = html.unescape(txt)
 
 
-def a_print(text):
-    """TODO: looks like textParser.S
-    :param text:
-    """
-    import re
-    text = text[re.search("</\s*?head\s*?>", text).end():]
+def a_gen(text):
     end = 0
     while end < len(text):
         y = re.search("<a(.|\s)*?</a>", text[end:])
         if not y:
             break
         else:
+            yield y.group()
             end += y.end()
-            sub = y.group()
-            f = re.search("href=\"(.*?)\"", sub)
-            if not f:
-                continue
-            url = f.group(1)
-            if not url.startswith("http"):
-                if url.startswith('/'):
-                    url = test_url+url[1:]
-                else:
-                    print("bad")
-            words = re.sub("<(.|\s)*?>", " ", sub).split()
-            len_w = len(words)
-            if len_w > 0:
-                print("{} {} {}\n".format(url, " ".join(words), len_w))
+
+
+def a_print(text):
+    """TODO: looks like textParser.S
+    :param text:
+    """
+    text = text[re.search("</\s*?head\s*?>", text).end():]
+
+    for link in a_gen(text):
+        print(link)
+        f = re.search("href=\"(.*?)\"", link)
+        if not f:
+            continue
+        url = f.group(1)
+        if not url.startswith("http"):
+            print("bad")
+        words = re.sub("<(.|\s)*?>", " ", link).split()
+        len_w = len(words)
+        if len_w > 0:
+            print("{} {} {}\n".format(url, " ".join(words), len_w))
 
 
 a_print(txt)
