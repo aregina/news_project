c433c1dc346c2ea6e07e7716ee2e29935d6f1718
Anton Ostapenko <olsnod@gmail.com>
2016-03-26 15:43:36 +0500

    parser mod

--- aParser.py
+++ aParser.py
@@ -9,12 +9,6 @@ import html
 import re
 
 
-test_url = "https://meduza.io/"
-
-txt = urlOpen.read(test_url)
-txt = html.unescape(txt)
-
-
 def a_gen(text):
     end = 0
     while end < len(text):
@@ -30,20 +24,29 @@ def a_print(text):
     """TODO: looks like textParser.S
     :param text:
     """
-    text = text[re.search("</\s*?head\s*?>", text).end():]
+    import urllib.parse as pr
+
+    text = text[re.search("</\s*?(head|HEAD)\s*?>", text).end():]
 
     for link in a_gen(text):
-        print(link)
         f = re.search("href=\"(.*?)\"", link)
         if not f:
             continue
         url = f.group(1)
         if not url.startswith("http"):
-            print("bad")
+            print(url[:10])
+
+        if url.startswith('/'):
+            url = pr.urljoin(test_url,url[1:])
+
         words = re.sub("<(.|\s)*?>", " ", link).split()
         len_w = len(words)
         if len_w > 0:
             print("{} {} {}\n".format(url, " ".join(words), len_w))
 
 
+test_url = "http://www.e1.ru/"
+
+txt = urlOpen.read(test_url)
+txt = html.unescape(txt)
 a_print(txt)
