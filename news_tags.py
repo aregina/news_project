e5f9dfce71e37cd78386e617ae51b5334727879b
konstov <konstov@gmail.com>
2016-03-28 09:00:43 +0500

    little changes

--- news_tags.py
+++ news_tags.py
@@ -1,28 +1,28 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 import re
 
 # Укажу число размеченных новостей и число новостей для маркировки
 marked = 505
 for_mark = 30
-name_of_column = 'text' # имя колонки с новостью
+name_of_column = 'text'  # имя колонки с новостью
 
 # Читаю размеченную часть csv
-Text = pd.read_csv('d.csv').ix[:marked+for_mark,0:6]
+Text = pd.read_csv('d.csv').ix[:marked + for_mark, 0:6]
 
-#приведу все новости к нижнему регистру
+# приведу все новости к нижнему регистру
 Text[name_of_column] = Text[name_of_column].apply(lambda x: re.sub('[^а-яА-Я]', ' ', x.lower()))
 
 # превращаю новость числовой вектор и обучаюсь на нём.
-Text_train = Text.ix[:marked,name_of_column]
+Text_train = Text.ix[:marked, name_of_column]
 
-tf = TfidfVectorizer(ngram_range = (1, 1))
+tf = TfidfVectorizer(ngram_range=(1, 1))
 X = tf.fit_transform(Text_train)
 
 # колонка с категориями
-y = Text.ix[:marked,'tag']
+y = Text.ix[:marked, 'tag']
 y = y.fillna('No tag')
 
 # обучу логистическую регрессию
@@ -30,17 +30,16 @@ y = y.fillna('No tag')
 LR = LogisticRegression(penalty='l2', C=100)
 LR.fit(X, y)
 
-
 # прочитаю их файла ещё несколько новостей и попробую поставить на них тэги
 
-X_test_raw = Text.ix[marked+1:marked+for_mark,name_of_column]
+X_test_raw = Text.ix[marked + 1:marked + for_mark, name_of_column]
 X_test = tf.transform(X_test_raw)
 
 #################################### Выбор просто топового тэга ##############################
 tags = LR.predict(X_test)
 
 # Создаю индексы и полученный прогноз тегов объёдиняю в один датафрейм
-index = [i for i in range(marked+1,marked+for_mark+1)]
+index = [i for i in range(marked + 1, marked + for_mark + 1)]
 tags = pd.DataFrame(tags, index=index)
 
 # Соединяю тэги и тексты новостей
@@ -57,9 +56,9 @@ top_3_tags = []
 for probas in res_proba:
     cat_and_prob = [pair for pair in zip(probas, LR.classes_)]
     cat_and_prob.sort()
-    top_3_tags.append([cat[1] for cat in cat_and_prob[-1:-4:-1] if cat[0] > 0.06]) #чтобы отсечь совсем маловероятные
-    
-index = [i for i in range(marked+1,marked+for_mark+1)]
+    top_3_tags.append([cat[1] for cat in cat_and_prob[-1:-4:-1] if cat[0] > 0.06])  # чтобы отсечь совсем маловероятные
+
+index = [i for i in range(marked + 1, marked + for_mark + 1)]
 tagged_news = pd.DataFrame(top_3_tags, index=index)
 
 # Соединяю тэги и тексты новостей
