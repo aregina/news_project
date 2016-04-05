2ae40f4693c006f54ae6f40e59715115b9c1e959
Anton Ostapenko <olsnod@gmail.com>
2016-04-05 09:55:00 +0500

    chenge method info

--- checkUrlDuplicate.py
+++ checkUrlDuplicate.py
@@ -11,10 +11,10 @@ for news in News.objects.iterator():
     url = aParser.remove_query_from_url(news.url)
     if len(url) != len(news.url):
         if not News.objects.filter(url=url)[:1].exists():
-            print("update news id"+str(news.id))
+            print("update news id{}\t{}".format(str(news.id),news.url))
             news.url = url
             news.save()
         else:
-            print("delete news id"+str(news.id))
+            print("delete news id{}\t{}".format(str(news.id),news.url))
             news.delete()
 
