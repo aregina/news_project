714e45b4d2a2e5f4c159b60dd63e7dcca6e27c36
Konstantin <konstov@users.noreply.github.com>
2016-04-06 10:19:19 +0500

    Update models.py
    add AllTags model for tags and modify NewsTags

--- db/models.py
+++ db/models.py
@@ -26,22 +26,20 @@ class NewsText(models.Model):
     )
     text = models.TextField()
 
-
 class KeyWord(models.Model):
     news = models.ManyToManyField(
             News)
     word = models.CharField(max_length=300)
 
 class NewsTags(models.Model):
-    news = models.ManyToManyField(
-            News)
-    tag1 = models.CharField(max_length=20)
-    tag2 = models.CharField(max_length=20)
-    tag3 = models.CharField(max_length=20)
-
+    news = models.ForeignKey(Site, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=20)
+    weight = models.DecimalField(max_digits=6, decimal_places=3)
 
 class ASources(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
     url = models.URLField(max_length=300)
 
-
+class AllTags(models.Model):
+    name = models.CharField(max_length=20)
+    
