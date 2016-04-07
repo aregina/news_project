745e2cf61b00baedd14df0ef79195b3ac9ac49e8
Konstantin <konstov@users.noreply.github.com>
2016-04-07 09:07:40 +0500

    Update tags_extractor.py
    Изменил как ты предложил.
    После просмотра новости меняю её флаг check_tag на

--- tags_extractor.py
+++ tags_extractor.py
@@ -8,19 +8,13 @@ from prjparser import news_tags
 
 
 def tags_create():
-    # тут лучше итерировать сразу NewsText
-    # можно в модель NewsText добавить булево поле (признак того что ты этот текст уже посмотрел)
-    # и сразу брать только непросмотренные тексты
-    #   NewsText.objects.filter(___=False).iterator():
-    for news in News.objects.iterator():
+
+    for news in NewsText.objects.filter(check_tag=False).iterator():
         tags_list = news_tags.get_tags(news.newstext.text)
         for tag in tags_list:
-            # тут нужно истользовать только один метод создания записи в базе
-            # или  nt = NewsTags(..)
-            #       nt.save()
-            # или NewsTags.objects.create()
             news_tag = NewsTags.objects.create(news=news, weight=tag[0], tag=tag[1])
-            news_tag.save()
+        news.check_tag = True
+        news.save()
 
 
 if __name__ == "__main__":
