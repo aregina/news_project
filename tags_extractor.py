8e2145bf7d05c702902d71ce85e60d2647718889
Anton Ostapenko <olsnod@gmail.com>
2016-04-07 11:17:21 +0500

    add new comment

--- tags_extractor.py
+++ tags_extractor.py
@@ -8,13 +8,17 @@ from prjparser import news_tags
 
 
 def tags_create():
+    # переменная news уже не news а news_text или что то вроде того и NewsText надо импортировать
     for news in NewsText.objects.filter(check_tag=False).iterator():
+                                    # у news_text.news_text нет text =)
         tags_list = news_tags.get_tags(news.newstext.text)
         for tag in tags_list:
             try:
+                # надо импортировать модель AllTags и AllTags.objects.get вернет не id а ссылку на обект
                 tag_id = AllTags.objects.get(tag=tag[1])
-                NewsTags.objects.create(news=news, weight=tag[0], tag=tag_id)
-            except:
+                NewsTags.objects.create(news=news, weight=tag[0], tag=tag_id) # эту сточку можно вынести за
+                                                                            # блок try и тогда не будет повтора
+            except AllTags.DoesNotExist:  # <- лучше всегда уточнать какую ошибку ты перехватываешь
                 tag_id = AllTags.objects.create(tag=tag[1])
                 NewsTags.objects.create(news=news, weight=tag[0], tag=tag_id)
                 
