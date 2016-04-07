7e8869fc7bffb4f589189ca775afd15f80331f31
Konstantin <konstov@users.noreply.github.com>
2016-04-07 12:46:54 +0500

    Update tags_extractor.py
    Импортировал NewsText, AllTags
    Вынес создание записи о весе новости за try ... except
    Кажется (гуглил, по запросу "default name of primary_key field in django" и его вариациях, но не совсем уверен) primary_key по умолчанию называется id (если верить стэку).

--- tags_extractor.py
+++ tags_extractor.py
@@ -3,24 +3,22 @@ import os, django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "news_project.settings")
 django.setup()
 
-from db.models import NewsTags, News
+from db.models import NewsTags, NewsText, AllTags
 from prjparser import news_tags
 
 
 def tags_create():
-    # переменная news уже не news а news_text или что то вроде того и NewsText надо импортировать
-    for news in NewsText.objects.filter(check_tag=False).iterator():
-                                    # у news_text.news_text нет text =)
-        tags_list = news_tags.get_tags(news.newstext.text)
+
+    for news_text in NewsText.objects.filter(check_tag=False).iterator():
+        tags_list = news_tags.get_tags(news_text.text)
         for tag in tags_list:
             try:
-                # надо импортировать модель AllTags и AllTags.objects.get вернет не id а ссылку на обект
-                tag_id = AllTags.objects.get(tag=tag[1])
-                NewsTags.objects.create(news=news, weight=tag[0], tag=tag_id) # эту сточку можно вынести за
-                                                                            # блок try и тогда не будет повтора
-            except AllTags.DoesNotExist:  # <- лучше всегда уточнать какую ошибку ты перехватываешь
-                tag_id = AllTags.objects.create(tag=tag[1])
-                NewsTags.objects.create(news=news, weight=tag[0], tag=tag_id)
+                tag_from_db = AllTags.objects.get(tag=tag[1])
+                tag_id = tag_from_db.id
+            except AllTags.DoesNotExist:
+                tag_from_db = AllTags.objects.create(tag=tag[1])
+                tag_id = tag_from_db.id
+            NewsTags.objects.create(news=news_text, weight=tag[0], tag=tag_id)
                 
         news.check_tag = True
         news.save()
