445e148aab1cf4867baf797961b2d651b5eef101
Anton Ostapenko <olsnod@gmail.com>
2016-04-07 16:51:32 +0500

    add comment

--- tags_extractor.py
+++ tags_extractor.py
@@ -18,10 +18,25 @@ def tags_create():
                 tag_from_db = AllTags.objects.create(tag=tag[1])
             tag_id = tag_from_db.id
             NewsTags.objects.create(news=news_text, weight=tag[0], tag=tag_id)
-                
+
         news.check_tag = True
         news.save()
 
 
 if __name__ == "__main__":
     tags_create()
+
+#
+# def tags_create():
+#
+#     for news_text in NewsText.objects.filter(check_tag=False).iterator():
+#         tags_list = news_tags.get_tags(news_text.text)
+#         for weight, tag in tags_list:
+#             try:
+#                 tag_from_db = AllTags.objects.get(tag=tag)
+#             except AllTags.DoesNotExist:
+#                 tag_from_db = AllTags.objects.create(tag=tag)
+#             NewsTags.objects.create(news=news_text, weight=weight, tag=tag_from_db)
+#
+#         news_text.news.check_tag = True
+#         news_text.news.save()
