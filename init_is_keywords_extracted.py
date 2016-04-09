be401648981e1edc77a6c929ac083cc03078c3a0
Anton Ostapenko <olsnod@gmail.com>
2016-04-09 13:08:28 +0500

    fix db merge

--- init_is_keywords_extracted.py
+++ init_is_keywords_extracted.py
@@ -6,10 +6,10 @@ django.setup()
 from db.models import News, NewsText
 from django.db import transaction
 
-# with transaction.atomic():
-for newstext in NewsText.objects.iterator():
-    if newstext.news.keyword_set.exists():
-        newstext.is_keywords_extracted = True
-        print("news id \t {}".format(newstext.news.pk))
-        newstext.save()
+with transaction.atomic():
+    for newstext in NewsText.objects.iterator():
+        if newstext.news.keyword_set.exists():
+            newstext.is_keywords_extracted = True
+            print("news id \t {}".format(newstext.news.pk))
+            newstext.save()
 
