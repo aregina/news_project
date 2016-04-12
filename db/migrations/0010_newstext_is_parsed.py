1bbf2a8127651b5e3816de72fe997a6dd9728a9c
Regina Arslanova <aregina@yandex.ru>
2016-04-12 21:01:50 +0500

    small changes

--- db/migrations/0010_newstext_is_parsed.py
+++ db/migrations/0010_newstext_is_parsed.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('db', '0009_auto_20160405_0949'),
+        ('db', '0009_newstext_is_keywords_extracted'),
     ]
 
     operations = [
