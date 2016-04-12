a1d2fc0537f15b95937adce05c7b4eeb778ae6a0
Regina Arslanova <aregina@yandex.ru>
2016-04-12 21:22:24 +0500

    change key_words.py

--- prjparser/key_words.py
+++ prjparser/key_words.py
@@ -3,6 +3,14 @@ from collections import Counter
 
 morph = pymorphy2.MorphAnalyzer()
 
+def get_key_words_count(news_words_count, title_words_count):
+    total_words_count = news_words_count + title_words_count
+    key_words_count = round((total_words_count/100)*10)
+    if key_words_count >= 10:
+        return key_words_count
+    else:
+        return 10
+
 
 def get_key_word(news, news_title):
     special_characters = '.,!()-"":;?«»'
@@ -38,9 +46,16 @@ def get_key_word(news, news_title):
         if word in counted_words:
             counted_words[word] *= 3
 
+    key_words_count = get_key_words_count(len(words_from_news), len(words_from_title))
+    all_words = counted_words.most_common(key_words_count)
+
+    # key_words = []
+    # for word in counted_words:
+    #     if counted_words[word] > 1:
+    #         key_words.append(word)
+
     key_words = []
-    for word in counted_words:
-        if counted_words[word] > 1:
-            key_words.append(word)
+    for word in all_words:
+        key_words.append(word[0])
 
     return (key_words)
