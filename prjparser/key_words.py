eebffbcbab730a7076b9f28d09d108ebbbc993b2
Regina Arslanova <aregina@yandex.ru>
2016-04-16 11:24:56 +0500

    added anti_key_words.txt

--- prjparser/key_words.py
+++ prjparser/key_words.py
@@ -1,25 +1,32 @@
 import pymorphy2
 from collections import Counter
+from textblob import TextBlob
 
 morph = pymorphy2.MorphAnalyzer()
 
 def get_key_words_count(news_words_count, title_words_count):
     total_words_count = news_words_count + title_words_count
     key_words_count = round((total_words_count/100)*10)
-    if key_words_count >= 10 and key_words_count >= 10 :
+    if key_words_count >= 10 and key_words_count >= 10:
         return key_words_count
     else:
         return 10
 
 
 def get_key_word(news, news_title):
-    special_characters = '.,!()-"":;?«»'
+    special_characters = '.,!()-"":;?«»—'
     for character in special_characters:
         news = news.replace(character, ' ')
         news_title = news_title.replace(character, ' ')
 
-    words_from_news = news.split()
-    words_from_title = news_title.split()
+    # words_from_news = news.split()
+    # words_from_title = news_title.split()
+
+    tb_news = TextBlob(news)
+    tb_news_title = TextBlob(news_title)
+
+    words_from_news = list(tb_news.words)
+    words_from_title = list(tb_news_title.words)
 
     # interesting_speach_parts = ['NOUN', 'ADJF', 'ADJS', 'VERB', 'NUMR', 'ADVB']
     interesting_speech_parts = ['NOUN', ]
@@ -54,8 +61,11 @@ def get_key_word(news, news_title):
     #     if counted_words[word] > 1:
     #         key_words.append(word)
 
+    with open('anti_key_words.txt') as anti_key_words_file:
+        anti_key_words = set(anti_key_words_file.read().splitlines())
+
     key_words = []
     for word in all_words:
-        key_words.append(word[0])
-
-    return (key_words)
+        if word not in anti_key_words:
+            key_words.append(word[0])
+    return key_words
