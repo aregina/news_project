e528b3ae69a3b3833dab47426fcb7e3b7d28a2d0
Regina Arslanova <aregina@yandex.ru>
2016-04-21 21:28:01 +0500

    changed text_preparer

--- prjparser/text_prerparer.py
+++ prjparser/text_prerparer.py
@@ -6,7 +6,19 @@ from textblob import TextBlob
 MORPH = pymorphy2.MorphAnalyzer()
 PUNCTUATION_SIMBOLS = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
 WHITE_SPACES = ' ' * len(PUNCTUATION_SIMBOLS)
-INTERESTING_SPEACH_PARTS = ['NOUN', 'ADJF', 'ADJS', 'VERB', 'NUMR', 'ADVB']
+INTERESTING_SPEACH_PARTS = [
+    'NOUN',
+    'ADJF',
+    'ADJS',
+    'VERB',
+    'NUMR',
+    'ADVB',
+    'COMP',
+    'INFN',
+    'PRTF',
+    'PRTS',
+    'GRND',
+]
 
 
 def text_preparer(text):
@@ -17,10 +29,11 @@ def text_preparer(text):
     words_from_news = list(tb_news.words)
 
     interesting_words_from_news = []
-    for speech_parts in INTERESTING_SPEACH_PARTS:
-        for word in words_from_news:
-            parsed_word = MORPH.parse(word)[0]
-            if speech_parts in parsed_word.tag:
-                interesting_words_from_news.append(parsed_word.normal_form)
+    for word in words_from_news:
+        parsed_word = MORPH.parse(word)[0]
+        if parsed_word.tag.POS in PUNCTUATION_SIMBOLS:
+            interesting_words_from_news.append(parsed_word.normal_form)
 
-    return interesting_words_from_news
+    plain_text = ' '.join(interesting_words_from_news)
+
+    return plain_text
