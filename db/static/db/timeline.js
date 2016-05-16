29ed9d1c342e14f501064f3fa7894b23acf7c4ac
Anton Ostapenko <olsnod@gmail.com>
2016-05-16 17:41:56 +0500

    changed drawing graphics on the home page

--- db/static/db/timeline.js
+++ db/static/db/timeline.js
@@ -1,28 +1,31 @@
 
+var isMouseClick = false,
+    startPosition = 0,
+    lastMouseDelta = 0,
+    parseDate = d3.time.format("%Y-%m-%d").parse;
+
+var svg = d3.select(".timeline").append("svg")
+    .attr("width", "100%")
+    .attr("height", "100%")
+
+var line = svg.append("line")
+    .attr("x1", "50%")
+    .attr("y1", "0")
+    .attr("x2", "50%")
+    .attr("y2", "100%");
+
+var container = svg.append('g');
+
 d3.json("/?json2", function (error, jData) {
 
-    var isMouseClick = false,
-        startPosition = 0,
-        lastMouseDelta = 0,
-        parseDate = d3.time.format("%Y-%m-%d").parse;
+    if (error) throw error;
 
-    var svg = d3.select(".timeline").append("svg")
-        .attr("width", "100%")
-        .attr("height", "100%")
-        .on("mousemove", mousemove)
+    svg.on("mousemove", mousemove)
         .on("mousedown", function () { isMouseClick = true; startPosition = d3.event.clientY; })
         .on("mouseup", function () { isMouseClick = false; lastMouseDelta += (startPosition - d3.event.clientY) * -1 });
 
-    var line = svg.append("line")
-        .attr("x1", "50%")
-        .attr("y1", "0")
-        .attr("x2", "50%")
-        .attr("y2", "100%");
-
-    var container = svg.append('g');
 
 
-    if (error) throw error;
     jData.forEach(function (d) {
         d.y = parseDate(d.y);
         d.cnt = d.cnt;
@@ -31,6 +34,11 @@ d3.json("/?json2", function (error, jData) {
     jData.sort(function (a, b) {
         return a.y - b.y;
     });
+
+    rect_width = d3.scale.linear().range([5, 45]);
+
+    rect_width.domain(d3.extent(jData, function (d) { return d.cnt; }));
+
     var options = {
         //year: 'numeric',
         month: 'long',
@@ -52,8 +60,11 @@ d3.json("/?json2", function (error, jData) {
         .selectAll("rect")
         .data(jData)
         .enter()
+        .append('a')
+        .attr('xlink:href', '/')
         .append("rect")
-        .attr("width", function (d, i) { return d.cnt / 10 + "px" })
+        .attr("width", function (d) { return rect_width(d.cnt)+"%" } )
+        .attr("class", "cnt_rect")
         .attr("height", "25px")
         .attr("x", "50%")
         .attr("y", function (d, i) { return i * 25 + "px" });
@@ -71,7 +82,12 @@ d3.json("/?json2", function (error, jData) {
         .attr("x", "40%")
         .attr("y", "55%");
 
-    var text = svg.append("text")
+    link_to_news = svg
+        .append('a')
+        .attr('xlink:href', '/')
+
+    text = link_to_news
+        .append("text")
         .attr("class", "title")
         .attr("x", "40%")
         .attr("y", "60%");
@@ -87,6 +103,11 @@ d3.json("/?json2", function (error, jData) {
         return n_text.length > 20 ? n_text.substring(0, 35) + "..." : n_text;
     }
 
+    function getUrl(id) {
+        if (id < 0 || id >= grap[0].length) return "/";
+        return "/news/id" + grap[0][id].__data__.i;
+    }
+
     function heightInPersent(d) { return d * 100 + "%"; }
 
     function mousemove() {
@@ -100,6 +121,8 @@ d3.json("/?json2", function (error, jData) {
             text_prev.text(getText(n_id - 1));
             text.text(getText(n_id));
             text_post.text(getText(n_id + 1));
+
+            link_to_news.attr("href", getUrl(n_id));
         }
     }
 })
