39e308764f8fca4a1c9f58158d7d446cd81936f1
Anton Ostapenko <olsnod@gmail.com>
2016-06-29 17:50:30 +0500

    ...

--- db/views.py
+++ db/views.py
@@ -118,7 +118,7 @@ def news3_detail(request, news_id=0):
 def related_news_json(request, news_id=0):
     main_news = get_object_or_404(News, pk=news_id)
     related_news = {"name": main_news.title, "children": []}
-    for news in main_news.related_news:
+    for news in main_news.related_news.all():
         related_news_info = {"name": news.title, "size": 6714}
         related_news["children"].append(related_news_info)
     # main_news = get_object_or_404(News, pk=news_id)
